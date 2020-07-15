85141ef2d84b8d25d9032617ed424ef2f6911896
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-15 19:55:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,8 +5,12 @@ second_number = 4
 
 sum = first_number + second_number
 
+<<<<<<< HEAD
 difference = first_number - second_number
 
 product = first_number * second_number
 
 quotient = first_number / second_number
+=======
+difference = first_number - second_number
+>>>>>>> b33343a22770ac46123782200ac7c9b4ff1cba78
